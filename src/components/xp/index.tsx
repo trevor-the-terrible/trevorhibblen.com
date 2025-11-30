d1766de740ddcbd91f175391aaeca3304404d257
trevor <trevorh@fastmail.com>
2025-11-30 11:53:53 -0800

    chore: sync resume
    chore: prettier

--- src/components/xp/index.tsx
+++ src/components/xp/index.tsx
@@ -1,57 +1,69 @@
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
-import BgNoise from '@/components/bg-noise';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import BgNoise from "@/components/bg-noise";
 
 export function Xp() {
   return (
     <Card
-        id="xp"
-        className="
+      id="xp"
+      className="
           text-pretty subpixel-antialiased
           my-6
           relative
         "
-      >
-        <CardHeader>
-          <CardTitle>
-            Experience
-          </CardTitle>
-        </CardHeader>
+    >
+      <CardHeader>
+        <CardTitle>Experience</CardTitle>
+      </CardHeader>
 
-        <CardContent
-          className="prose dark:prose-invert"
-        >
-          <p>
-          An experienced full-stack developer. I’ve successfully managed and developed complete platforms and feature-sets from zero to production
-across a range of technologies. I value good design fundamentals, understanding the customer and product, and being an uplifting force to
-those around me. Quite comfortable in fast-paced environments and good taking on multiple roles, from gathering requirements, all aspects of
-design and development, to addressing stakeholder feedback and customer support. Also, kinda fun to work with 🎉
-          </p>
+      <CardContent className="prose dark:prose-invert">
+        <p>
+          Experienced full-stack developer. I’ve successfully managed and
+          developed complete platforms and feature-sets from zero to production
+          across a wide range of technologies. Quite comfortable in fast-paced
+          environments and good taking on multiple roles, from gathering
+          requirements, mentoring new developers, all aspects of design and
+          development (ui/ux/client/server/database structure), to addressing
+          stakeholder feedback and customer support. I value good design
+          fundamentals, understanding the customer and product, and being an
+          uplifting force to those around me.
+        </p>
 
-          <h4>About this site</h4>
-          <p>
-            Developed in Astro, with React components.
-
-            Most of the sections were fairly easy to implement.
-
-            The most challenging portion was the Feedback form.
-
-            <h5 className="m-0">Technologies used</h5>
-            <ul className="tracking-tight mt-0">
-              <li className="m-0 p-0">
-                <a href="https://astro.build/" target="astro.build">Astro</a>
-              </li>
-              <li className="m-0 p-0">
-                <a href="https://react.dev/" target="react.dev">React</a>
-              </li>
-              <li className="m-0 p-0">
-                <a href="https://tailwindcss.com/" target="tailwindcss.com">Tailwind</a>
-              </li>
-              <li className="m-0 p-0">
-                <a href="https://ui.shadcn.com/" target="_blank">Shadcn</a>
-              </li>
-            </ul>
-          </p>
-        </CardContent>
+        <h4>About this site</h4>
+        <p>
+          Developed in Astro, with React components. Most of the sections were
+          fairly easy to implement. The most challenging portion was the
+          Feedback form.
+          <h5 className="m-0">Technologies used</h5>
+          <ul className="tracking-tight mt-0">
+            <li className="m-0 p-0">
+              <a href="https://astro.build/" target="astro.build">
+                Astro
+              </a>
+            </li>
+            <li className="m-0 p-0">
+              <a href="https://react.dev/" target="react.dev">
+                React
+              </a>
+            </li>
+            <li className="m-0 p-0">
+              <a href="https://tailwindcss.com/" target="tailwindcss.com">
+                Tailwind
+              </a>
+            </li>
+            <li className="m-0 p-0">
+              <a href="https://ui.shadcn.com/" target="_blank">
+                Shadcn
+              </a>
+            </li>
+          </ul>
+        </p>
+      </CardContent>
     </Card>
-  )
+  );
 }
